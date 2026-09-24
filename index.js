dab2d13a05c3a80c3fa7cd3d7e485003f075f546
Ryane Oliveira <ryane.vasconcelos1@aluno.ce.gov.br>
2026-09-24 11:24:26 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const colheresArroz = 2
+const colheresFeijao = 2
+const colheresFarinha = 1
+const colheresMistura = 1
+
+let quantidadeArroz
+
+if (colheresArroz <= 2) {
+    quantidadeArroz = "Você comeu bem, não desperdiçou nada"
+} else {
+    quantidadeArroz = "Você colocou mais do que deveria, desperdiçou comida"
+}
+
+let quantidadeFeijao
+
+if (colheresFeijao <= 2) {
+    quantidadeFeijao = "Você comeu bem, não desperdiçou nada"
+} else {
+    quantidadeFeijao = "Você colocou mais do que deveria, desperdiçou comida"
+}
+
+let quantidadeFarinha
+
+if (colheresFarinha <= 1) {
+    quantidadeFarinha = "Você comeu bem, não desperdiçou nada"
+} else {
+    quantidadeFarinha = "Você colocou mais do que deveria, desperdiçou comida"
+}
+
+let quantidadeMistura
+
+if (colheresMistura <= 1) {
+    quantidadeMistura = "Você comeu bem, não desperdiçou nada"
+} else {
+    quantidadeMistura = "Você colocou mais do que deveria, desperdiçou comida"
+}
